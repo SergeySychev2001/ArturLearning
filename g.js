4ce889d1c27261635fb2afff789b483fa8e4375d
s.sychyov <s.sychyov@global-system.ru>
2024-07-10 00:55:22 +0300

    init

---
+++ g.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// Путь к выбранной папке
+const parentFolder = 'D:\\Education\\ArturLearning\\JS\\';
+
+const pF = [
+
+];
+
+
+for (let i = 1; i <= 50; i++) {
+    const folderName = `${i}`;
+
+    for (let j = 0; j < pF.length; j++) {
+        const folderPath = path.join(parentFolder + pF[j], folderName);
+        fs.mkdirSync(folderPath, { recursive: true });
+    }
+
+
+}
+
+console.log('Папки были успешно созданы.');
